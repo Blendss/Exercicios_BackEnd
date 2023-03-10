06ec01a5e9b1a8764fd43972b715d6b5b8edd42d
Thomas Buchser Monteiro <111438407+Blendss@users.noreply.github.com>
2023-03-10 19:47:26 -0300

    Add files via upload

---
+++ Ex2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exerc2B
+{
+    public static void Main()
+    {
+        int x = 0;
+        Console.WriteLine("Insira o numero de convidados: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        string[] convidados = new string[x];
+        int i = 0;
+        int a = 0;
+
+        do
+        {
+            Console.WriteLine("Insira o nome do convidado: ");
+            convidados[i] = Convert.ToString(Console.ReadLine());
+            i++;
+        } while (i < convidados.Length);
+
+        Console.WriteLine("Lista de convidados: ");
+        do
+        {
+            Console.WriteLine(convidados[a]);
+            a++;
+        } while (a < convidados.Length);
+
+
+    }
+}
