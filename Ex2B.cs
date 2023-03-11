bc0f9c41c5519958dcb7ed35d6d162a4906d8255
Thomas Buchser Monteiro <111438407+Blendss@users.noreply.github.com>
2023-03-10 21:56:28 -0300

    Add files via upload

---
+++ Ex2B.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exerc2B
+{
+    public static void Main()
+    {
+        List<string> convidados = new List<string>();
+
+        do
+        {
+            Console.WriteLine("digite SAIR para parar\n Digite o nome do convidado:  ");
+            convidados.Add(Convert.ToString(Console.ReadLine()));
+
+            if (convidados.Contains("SAIR") || convidados.Contains("sair") || convidados.Contains("Sair"))
+            {
+                break;
+            }
+
+        } while (true);
+
+        Console.WriteLine("A lista possui: " + convidados.Count + " convidados");
+        Console.WriteLine(string.Join("\n", convidados));
+
+    }
+}
---
+++ Ex3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string frutasStr = "maçã, banana, laranja, maçã, pêra, banana, uva, uva";
+
+        string[] frutasArr = frutasStr.Split(new[] { ", " }, StringSplitOptions.None);
+        List<string> frutas = new List<string>(new HashSet<string>(frutasArr));
+
+        List<string> frutasMaiusculas = new List<string>();
+
+        foreach (string fruta in frutas)
+        {
+            frutasMaiusculas.Add(char.ToUpper(fruta[0]) + fruta.Substring(1));
+        }
+        Console.WriteLine("Lista de Frutas formatada:");
+        foreach (string fruta in frutasMaiusculas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
